c3fc4e180e196930e3263b47e0e4de8b4018c6d6
Henrique Mendes <henrique.mendes@mercadolivre.com>
2020-11-17 22:44:27 -0300

    implement signup endpoint and user schema validation

---
+++ src/models/User.js
@@ -0,0 +1,75 @@
+import { Schema, model } from 'mongoose';
+import joi from 'joi';
+
+import ApplicationError from '../errors/ApplicationError';
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String, required: true, min: 5, max: 100,
+    },
+    email: {
+      type: String, required: true, min: 5, max: 100,
+    },
+    password: {
+      type: String, required: true, min: 5, max: 200,
+    },
+    imageURL: {
+      type: String, default: 'https://www.ecp.org.br/wp-content/uploads/2017/12/default-avatar.png',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+class UserEntity {
+  constructor() {
+    this.User = model('User', userSchema);
+
+    this.fullName = joi.string().min(5).max(100).required();
+    this.email = joi.string().email().min(5).max(100)
+      .required();
+    this.password = joi.string().min(5).max(100).required();
+    this.imageURL = joi.string().min(5).max(200);
+
+    this.validateSignupParams = this.validateSignupParams.bind(this);
+    this.validateLoginParams = this.validateLoginParams.bind(this);
+  }
+
+  validateSignupParams(req, res, next) {
+    const signupUserSchema = joi.object({
+      fullName: this.fullName,
+      email: this.email,
+      password: this.password,
+      imageURL: this.imageURL,
+    }).options({ abortEarly: false });
+
+    const joiValidation = signupUserSchema.validate(req.body);
+
+    if (joiValidation.error) {
+      const errorObject = joiValidation.error.details.reduce((acc, error) => {
+        acc[error.context.label] = error.message;
+
+        return acc;
+      }, {});
+
+      throw new ApplicationError({ message: errorObject, type: 'Auth-Signup-Validation-Error', status: 400 });
+    }
+
+    return next();
+  }
+
+  validateLoginParams(req, res, next) {
+    const loginUserSchema = joi.object({
+      email: this.email,
+      password: this.password,
+    });
+
+    const joiValidation = loginUserSchema.validate(req.body);
+
+    console.log(joiValidation.error.details);
+  }
+}
+
+export default new UserEntity();
---
+++ src/routes/authRoutes/public/routes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import UserEntity from '../../../models/User';
+import authService from '../../../services/auth.service';
+import ApplicationError from '../../../errors/ApplicationError';
+
+const router = Router();
+
+router.post('/signup', UserEntity.validateSignupParams, async (req, res, next) => {
+  try {
+    const { body } = req;
+
+    await authService.register(body);
+
+    return res.status(201).json({ message: 'User created' });
+  } catch (error) {
+    return next(new ApplicationError(error));
+  }
+});
+
+export default router;
