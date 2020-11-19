29b03715bb669b9ede64db751abba5bfe3420318
unimatrix2 <dnny.nis@gmail.com>
2020-11-18 23:29:38 -0300

    resolve conflicts

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -5,55 +5,55 @@ import passwordUtils from '../utils/password.utils';
 import ApplicationError from '../errors/ApplicationError';
 
 class AuthService {
-    constructor() {
-        this.authRepo = authRepository;
+  constructor() {
+    this.authRepo = authRepository;
+  }
+
+  async register(body) {
+    try {
+      await this.vefifyExistentUser(body);
+
+      const newUser = { ...body, password: passwordUtils.encrypt(body.password) };
+
+      await this.authRepo.saveUser(newUser);
+    } catch (error) {
+      throw new ApplicationError(error);
     }
+  }
 
-    async register(body) {
-        try {
-            await this.vefifyExistentUser(body);
-
-            const newUser = { ...body, password: passwordUtils.encrypt(body.password) };
-
-            await this.authRepo.saveUser(newUser);
-        } catch (error) {
-            throw new ApplicationError(error);
-        }
-    }
-
-    async authenticateUser(userCredentials) {
+  async authenticateUser(userCredentials) {
     // buscar user pelo email (jogar um erro caso nao encontre)
     // validar a senha enviada com a senha que veio do banco usando o bcrypt
     // autentica o usuario (gerar  uma token JWT e retornar no response do controller)
 
-        const userFromDb = await this.authRepo.findUser(userCredentials.email);
+    const userFromDb = await this.authRepo.findUser(userCredentials.email);
 
-        if (!userFromDb) {
-            throw new ApplicationError({ message: 'Wrong Credentials', type: 'Auth-Login-Invalid-Credentials', status: 400 });
-        }
-
-        const isPasswordValid = passwordUtils.verify(userCredentials.password, userFromDb.password);
-
-        if (!isPasswordValid) {
-            throw new ApplicationError({ message: 'Wrong Credentials', type: 'Auth-Login-Invalid-Credentials', status: 400 });
-        }
-
-        const token = jwt.sign(
-            { id: userFromDb._id },
-            process.env.TOKEN_SECRET,
-            { expiresIn: '15s' },
-        );
-
-        return token;
+    if (!userFromDb) {
+      throw new ApplicationError({ message: 'Wrong Credentials', type: 'Auth-Login-Invalid-Credentials', status: 400 });
     }
 
-    async vefifyExistentUser({ email }) {
-        const user = await this.authRepo.findUser(email);
+    const isPasswordValid = passwordUtils.verify(userCredentials.password, userFromDb.password);
 
-        if (user) {
-            throw new ApplicationError({ message: 'User already exists', type: 'Auth-Signup', status: 400 });
-        }
+    if (!isPasswordValid) {
+      throw new ApplicationError({ message: 'Wrong Credentials', type: 'Auth-Login-Invalid-Credentials', status: 400 });
     }
+
+    const token = jwt.sign(
+      { id: userFromDb._id },
+      process.env.TOKEN_SECRET,
+      { expiresIn: '15s' },
+    );
+
+    return token;
+  }
+
+  async vefifyExistentUser({ email }) {
+    const user = await this.authRepo.findUser(email);
+
+    if (user) {
+      throw new ApplicationError({ message: 'User already exists', type: 'Auth-Signup', status: 400 });
+    }
+  }
 }
 
 export default new AuthService();
