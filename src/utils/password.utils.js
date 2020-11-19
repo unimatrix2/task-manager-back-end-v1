4084ff7d99f5088cdbde93a4ff7aaa10ea08f520
unimatrix2 <dnny.nis@gmail.com>
2020-11-18 23:30:07 -0300

    resolve conflicts

--- src/utils/password.utils.js
+++ src/utils/password.utils.js
@@ -1,18 +1,18 @@
 import bcrypt from 'bcrypt';
 
 class PasswordUtils {
-    constructor() {
-        this.bcrypt = bcrypt;
-        this.saltRounds = 10;
-    }
+  constructor() {
+    this.bcrypt = bcrypt;
+    this.saltRounds = 10;
+  }
 
-    encrypt(plainTextPassword) {
-        return this.bcrypt.hashSync(plainTextPassword, this.saltRounds);
-    }
+  encrypt(plainTextPassword) {
+    return this.bcrypt.hashSync(plainTextPassword, this.saltRounds);
+  }
 
-    verify(plainTextPassword, encryptedPassword) {
-        return this.bcrypt.compareSync(plainTextPassword, encryptedPassword);
-    }
+  verify(plainTextPassword, encryptedPassword) {
+    return this.bcrypt.compareSync(plainTextPassword, encryptedPassword);
+  }
 }
 
 export default new PasswordUtils();
