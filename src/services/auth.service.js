e20e14d069e90f84d47e8bcd144317dfb13a90d7
unimatrix2 <dnny.nis@gmail.com>
2020-11-18 23:27:46 -0300

    resolved conflicts

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken';
+
 import authRepository from '../repository/auth.repository';
 import passwordUtils from '../utils/password.utils';
 import ApplicationError from '../errors/ApplicationError';
@@ -17,6 +19,32 @@ class AuthService {
         } catch (error) {
             throw new ApplicationError(error);
         }
+    }
+
+    async authenticateUser(userCredentials) {
+    // buscar user pelo email (jogar um erro caso nao encontre)
+    // validar a senha enviada com a senha que veio do banco usando o bcrypt
+    // autentica o usuario (gerar  uma token JWT e retornar no response do controller)
+
+        const userFromDb = await this.authRepo.findUser(userCredentials.email);
+
+        if (!userFromDb) {
+            throw new ApplicationError({ message: 'Wrong Credentials', type: 'Auth-Login-Invalid-Credentials', status: 400 });
+        }
+
+        const isPasswordValid = passwordUtils.verify(userCredentials.password, userFromDb.password);
+
+        if (!isPasswordValid) {
+            throw new ApplicationError({ message: 'Wrong Credentials', type: 'Auth-Login-Invalid-Credentials', status: 400 });
+        }
+
+        const token = jwt.sign(
+            { id: userFromDb._id },
+            process.env.TOKEN_SECRET,
+            { expiresIn: '15s' },
+        );
+
+        return token;
     }
 
     async vefifyExistentUser({ email }) {
