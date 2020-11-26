d4695b8a1a93372b81b07f33208d5370fce43281
Danny <62706508+unimatrix2@users.noreply.github.com>
2020-11-26 17:38:57 -0300

    fixed conflicts

--- src/services/projects.service.js
+++ src/services/projects.service.js
@@ -3,43 +3,43 @@ import projectsRepository from '../repository/projects.repository';
 import ApplicationError from '../errors/ApplicationError';
 
 class ProjectsService {
-    constructor(projectsRepo) {
-        this.projectRepository = projectsRepo;
+  constructor(projectsRepo) {
+    this.projectRepository = projectsRepo;
+  }
+
+  async get(id, search) {
+    try {
+      const projectsFromDb = await this.projectRepository.get(id, search);
+
+      return projectsFromDb;
+    } catch (error) {
+      throw new ApplicationError({ message: error.message, type: 'Projects - Get Method', status: 502 });
     }
+  }
 
-    async get(search) {
-        try {
-            const projectsFromDb = await this.projectRepository.get(search);
+  async getOne(id) {
+    const projectFromDb = await this.projectRepository.getOne(id);
 
-            return projectsFromDb;
-        } catch (error) {
-            throw new ApplicationError({ message: error.message, type: 'Projects - Get Method', status: 502 });
-        }
+    return projectFromDb;
+  }
+
+  async create(newProject, id) {
+    await this.projectRepository.create(newProject, id);
+  }
+
+  async updateOne(updateObject, id) {
+    try {
+      const updatedProject = await this.projectRepository.updateOne(updateObject, id);
+
+      return updatedProject;
+    } catch (error) {
+      throw new ApplicationError({ message: error.message, status: 504 });
     }
+  }
 
-    async getOne(id) {
-        const projectFromDb = await this.projectRepository.getOne(id);
-
-        return projectFromDb;
-    }
-
-    async create(newProject) {
-        await this.projectRepository.create(newProject);
-    }
-
-    async updateOne(updateObject, id) {
-        try {
-            const updatedProject = await this.projectRepository.updateOne(updateObject, id);
-
-            return updatedProject;
-        } catch (error) {
-            throw new ApplicationError({ message: error.message, status: 504 });
-        }
-    }
-
-    async deleteOne(id) {
-        await this.projectRepository.deleteOne(id);
-    }
+  async deleteOne(id) {
+    await this.projectRepository.deleteOne(id);
+  }
 }
 
 export default new ProjectsService(projectsRepository);
